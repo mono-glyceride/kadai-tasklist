18264acd9c1b2235580f18abf3fab5f94ff6d9ac
gurido <glyceride.mono@gmail.com>
2021-08-18 14:30:03 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,12 +10,17 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- タスク作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ユーザ登録ページへのリンク --}}
-                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                @if (Auth::check())
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
                     {{-- ログインページへのリンク --}}
-                    <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,13 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+   <!-- ここにページ毎のコンテンツを書く -->
 
-<!-- ここにページ毎のコンテンツを書く -->
-
-
+    @if (Auth::check())
 <h1>タスク一覧</h1>
-{{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
@@ -32,5 +29,15 @@
     
     {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
-
+    
+    @else
+    <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the TaskList</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    
+     @endif
 @endsection
